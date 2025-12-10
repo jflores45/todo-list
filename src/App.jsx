57fcdcb8b4ac9df71b4688dc32df88af8683b2f1
jflores45 <jahaira@umich.edu>
2025-12-09 19:56:38 -0600

    local changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import './App.css';
+import { useState } from 'react';
 import TodoList from './TodoList';
 import TodoForm from './TodoForm';
+
 function App() {
-  
+  const [newTodo, setNewTodo] = useState("Example Text")
+  const todos = [
+    { id: 1, title: "review resources" },
+    { id: 2, title: "take notes" },
+    { id: 3, title: "code out app" }
+  ];
   return (
     <div>
-      <h1>My Todos</h1>
-      <TodoForm/>
-      <TodoList/>
+        <h1>My Todos</h1>
+        <TodoForm/>
+
+        <p>{newTodo}</p>
+
+        <TodoList todos={todos} />
     </div>
   );
 }
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,19 +1,13 @@
+import TodoListItem from "./TodoListItem";
 
-
-function TodoList(){
-    
-    const todos = [
-        {id: 1, title: "review resources"},
-        {id: 2, title: "takes notes"},
-        {id: 3, title: "code out app"}
-    ];
-    return(
-        <ul>
-            {todos.map(todo => (
-            <li key={todo.id}>{todo.title} </li>
-            ))}
-        </ul>
-    );
+function TodoList({ todos }) {
+  return (
+    <ul>
+      {todos.map(todo => (
+        <TodoListItem key={todo.id} todo={todo} />
+      ))}
+    </ul>
+  );
 }
 
 export default TodoList;
