f4fa2b80e901165ebea3cb28632be6d7862bd68e
jflores45 <jahaira@umich.edu>
2025-12-13 10:44:43 -0600

    Update todo array

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,12 @@ import TodoForm from './TodoForm';
 
 function App() {
   const [newTodo, setNewTodo] = useState("Example Text")
-  const todos = [
-    { id: 1, title: "review resources" },
-    { id: 2, title: "take notes" },
-    { id: 3, title: "code out app" }
-  ];
   return (
     <div>
         <h1>My Todos</h1>
         <TodoForm/>
-
         <p>{newTodo}</p>
-
-        <TodoList todos={todos} />
+        <TodoList />
     </div>
   );
 }
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,6 +1,11 @@
 import TodoListItem from "./TodoListItem";
 
-function TodoList({ todos }) {
+function TodoList() {
+  const todos = [
+    { id: 1, title: "review resources" },
+    { id: 2, title: "take notes" },
+    { id: 3, title: "code out app" }
+  ];
   return (
     <ul>
       {todos.map(todo => (
